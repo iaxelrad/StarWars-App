5fa51dc91591d932f73f910a33cf4856f2994790
Itamar Axelrad <iaxelrad@gmail.com>
2021-03-23 22:43:36 +0200

    setting app background color.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Container } from './components/Container';
 import styled from 'styled-components';
-import { DARK_BLUE_1 } from './common/colors';
 
 export const App = () => {
   return (
@@ -16,7 +15,6 @@ export default App;
 const Wrapper = styled.div`
   display: flex;
   justify-content: center;
-  background-color: ${DARK_BLUE_1};
   font-family: 'Poppins, sans-serif';
   color: white;
 `;
